306a96ef071c84a16114ed57e24688809a241df3
Angello Nunez <angellonzgz@gmail.com>
2023-05-08 02:01:52 -0400

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+    return newString;
 };
+
+/* Another solution
+const reverseString = function (string) {
+    return string.split("").reverse().join("");
+};
+*/
+
 
 // Do not edit below this line
 module.exports = reverseString;
