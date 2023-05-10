009dffe942265512e24a8c7e8d06cb99b7e8a0ad
Angello Nunez <angellonzgz@gmail.com>
2023-05-10 01:09:28 -0400

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(initialNumber, finalNumber) {
+    let finalSum = 0;
+    if (initialNumber > finalNumber) {
+        let newNumber = finalNumber;
+        finalNumber = initialNumber;
+        initialNumber = newNumber;
+    }
 
+    if (typeof initialNumber !== "number" || typeof finalNumber  !== "number" || initialNumber < 0 || finalNumber < 0) {
+        return "ERROR";
+    }
+
+    for (let i = initialNumber; i <= finalNumber; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
