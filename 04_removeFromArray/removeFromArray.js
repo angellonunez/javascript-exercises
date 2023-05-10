a4443739f8f2fb1ea35e98af21a0857e5bb9cbc1
Angello Nunez <angellonzgz@gmail.com>
2023-05-10 00:43:54 -0400

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removed) {
+    let newArray = [];
+    array.forEach((item) => {
+        if (!removed.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
